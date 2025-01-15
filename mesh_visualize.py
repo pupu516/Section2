80c8f60f84c014af33722ba85a0d59ad3129da5b
zhuoruli <zhuoruli516@gmail.com>
2025-01-15 00:03:23 +0000

    (a)

---
+++ mesh_visualize.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+file_path = 'mesh.dat'
+data = pd.read_csv(file_path, delimiter=' ', header=0)
+
+print(data.head())
+
+plt.plot(data['X'], data['Y'])
+plt.xlabel('X')
+plt.ylabel('Y')
+
+
+output_file = 'mesh_plot.png'
+plt.savefig(output_file)
+
+print('graph generated!')
